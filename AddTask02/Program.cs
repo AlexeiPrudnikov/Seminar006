43e298873c0a5c953cf92fee6dbfe34d18ad1bd0
Alexei <alexei.prudnikov@gmail.com>
2022-03-29 14:28:43 +0300

    Add AddTask02

---
+++ AddTask02/Program.cs
@@ -0,0 +1,41 @@
+﻿int GetCountVowel(string str)
+{
+    int countVowel = 0;
+    if (str.Length == 0) return 0;
+    if (str.Length > 1)
+    {
+        countVowel += GetCountVowel(str.Substring(1,str.Length - 1));
+    }
+    switch (str[0])
+    {
+        case 'а':
+        case 'я':
+        case 'о':
+        case 'ё':
+        case 'у':
+        case 'ю':
+        case 'ы':
+        case 'и':
+        case 'э':
+        case 'е':
+        case 'А':
+        case 'Я':
+        case 'О':
+        case 'Ë':
+        case 'У':
+        case 'Ю':
+        case 'Ы':
+        case 'И':
+        case 'Э':
+        case 'Е':
+                countVowel++;
+                break;
+    }
+    return countVowel;
+}
+Console.Clear();
+Console.WriteLine("==========Дополнительная задача № 2==========");
+Console.WriteLine("На вход подаётся поговорка “без труда не выловишь и рыбку из пруда”. Используя рекурсию, подсчитайте, сколько в поговорке гласных букв");
+string str = "без труда не выловишь и рыбку из пруда";
+Console.WriteLine($"Исходная строка: {str}");
+Console.WriteLine($"Количесво гласных в исходной строке -> {GetCountVowel(str)}");
